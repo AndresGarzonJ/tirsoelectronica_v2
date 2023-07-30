0b375a39d2f37a319ba069c7d1e74e57c0a06804
Andres Garzon <sebasgarzonj@gmail.com>
2023-07-30 15:19:07 -0500

    [Feat] Add contact.name_headquarter in db

--- project/app/Models/Contact/Contact.php
+++ project/app/Models/Contact/Contact.php
@@ -2,33 +2,41 @@
 
 namespace App\Models\Contact;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
-{   
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
+{
+    use HasFactory;
     protected $table = 'contact';
-
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    
     protected $fillable = [
         'cover',
-        'name_proprietary',  
+        'name_proprietary',
         'name_enterprise',
-        'description', 
+        'name_headquarter',
+        'description',
         'address',
         'e_mail',
         'telephone_number_1',
         'telephone_number_2',
         'telephone_number_3',
-        'profile_youtube',
-        'profile_twitter',
-        'profile_mercadolibre',
         'profile_facebook',
-             
+        'profile_twitter',
+        'profile_youtube',
+        'profile_mercadolibre',
     ];
-    protected $hidden = []; 
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
 }
-  
