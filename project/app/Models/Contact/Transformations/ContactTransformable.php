83defe802d14f2c6dedbd608ebd4d2b069410328
Andres Garzon <sebasgarzonj@gmail.com>
2023-07-30 15:20:28 -0500

    [Refactor] Edit  ContactTransformable

--- project/app/Models/Contact/Transformations/ContactTransformable.php
+++ project/app/Models/Contact/Transformations/ContactTransformable.php
@@ -3,10 +3,9 @@
 namespace App\Models\Contact\Transformations;
 
 use App\Models\Contact\Contact;
-use Illuminate\Support\Facades\Storage;
- 
+
 trait ContactTransformable
-{ 
+{
     /**
      * Transform the contact
      *
@@ -15,25 +14,42 @@ trait ContactTransformable
      */
     protected function transformContact(Contact $contact)
     {
-        $file = Storage::disk('public')->exists($contact->cover) ? $contact->cover : null;
+        $cont = new Contact;
+        $cont->id = (int) $contact->id;
+        $cont->name_proprietary = $contact->name_proprietary;
+        $cont->cover = $this->rewriteExitsImagePathContact($contact->cover);
+        $cont->name_enterprise = $contact->name_enterprise;
+        $cont->name_headquarter = $contact->name_headquarter;
+        $cont->description = $contact->description;
+        $cont->address = $contact->address;
+        $cont->e_mail = $contact->e_mail;
+        $cont->telephone_number_1 = $contact->telephone_number_1;
+        $cont->telephone_number_2 = $contact->telephone_number_2;
+        $cont->telephone_number_3 = $contact->telephone_number_3;
+        $cont->profile_facebook = $contact->profile_facebook;
+        $cont->profile_twitter = $contact->profile_twitter;
+        $cont->profile_youtube = $contact->profile_youtube;
+        $cont->profile_mercadolibre = $contact->profile_mercadolibre;
+        return $cont;
+    }
 
-        $contc = new Contact;
-        $contc->id = (int) $contact->id;
-        $contc->name_proprietary = $contact->name_proprietary;
-        $contc->cover = $file;
-        $contc->name_enterprise = $contact->name_enterprise;
-        $contc->description = $contact->description;
-        $contc->address = $contact->address;
-        $contc->e_mail = $contact->e_mail;
-        $contc->telephone_number_1 = $contact->telephone_number_1;
-        $contc->telephone_number_2 = $contact->telephone_number_2;
-        $contc->telephone_number_3 = $contact->telephone_number_3;
-        $contc->profile_facebook = $contact->profile_facebook;
-        $contc->profile_twitter = $contact->profile_twitter;
-        $contc->profile_youtube = $contact->profile_youtube;
-        $contc->profile_mercadolibre = $contact->profile_mercadolibre;
-
-
-        return $contc;
+    /**
+     * it checks the image path which registered to DB and it exists whether on storage. 
+     * if exist, return original path add asset. 
+     * if not exist, return path for No Data.png
+     * 
+     * @param string $path
+     * @return string $existsPath
+     */
+    private function rewriteExitsImagePathContact($path)
+    {
+        if ($path == null) {
+            return $path;
+        }
+        // if (file_exists("/var/www/storage/app/public/" . $path)) {
+        if (file_exists("D:/tirsoelectronica_v2/project/storage/app/public/" . $path)) {
+            return asset("storage/$path");
+        }
+        return asset("images/NoData.png");
     }
 }
